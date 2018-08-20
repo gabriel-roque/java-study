25702e42dd2fe21a84611cf85955c49dab8d864f
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-20 11:06:37 -0300

    update code

--- estruturado/src/HelloWolrd.java
+++ estruturado/src/HelloWolrd.java
@@ -1,5 +1,9 @@
-public class HelloWolrd {
+//classe principal
+public class HelloWolrd{
+    //metodo principal
     public static void main(String[] args) {
+
         System.out.println("Hello Wolrd!");
+
     }
 }
