b6308ea3d9bd3088442fe1807b5ed776d218434e
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-25 15:05:39 -0300

    Conversao String/int/float

---
+++ estruturado/src/String_Int.java
@@ -0,0 +1,18 @@
+public class String_Int {
+    public static void main(String[] args) {
+//        String to int
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+
+        System.out.println(valor);
+
+
+//        String to Float
+
+        String valor2 = "12.9";
+        float num = Float.parseFloat(valor2);
+
+        System.out.println(valor2);
+
+    }
+}
