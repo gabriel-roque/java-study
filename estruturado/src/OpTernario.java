210bbc26b51cdee7b10d002beb0e2ec14d8f6f0e
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-26 00:21:57 -0300

    op ternario

---
+++ estruturado/src/OpTernario.java
@@ -0,0 +1,13 @@
+public class OpTernario {
+    public static void main(String[] args) {
+        int n1 = 8;
+        int n2 = 6;
+        int r = 0;
+
+        r = (n1>n2)?0:1;
+
+        System.out.println(r);
+
+
+    }
+}
