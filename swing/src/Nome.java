d05655581f8e58ecb572ec7dc7488c0a4d96b1d2
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-26 00:23:20 -0300

    up code

--- swing/src/Nome.java
+++ swing/src/Nome.java
@@ -1,8 +1,8 @@
-import javax.swing.;
+import javax.swing.JOptionPane;
 
 public class Nome {
     public static void main(String[] args) {
-        String nome = JOptionPane.showInputDialog(null, "Insira seu nome")
+        String nome = JOptionPane.showInputDialog(null, "Insira seu nome");
 
     }
 }
