960718fcbebc83e5622904ba8e4a5a3a39d39357
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-28 21:11:53 -0300

    Chamando Classes exteriores

---
+++ chamandoClasses/src/Home.java
@@ -0,0 +1,9 @@
+public class Home {
+    public static void main(String[] args) {
+        /* chamando classe exterior
+            se faz necessario especificar o nome da classe
+            e o metodo que queremos solicitar
+         */
+        int r = Operacao.soma(22, 33);
+    }
+}
---
+++ chamandoClasses/src/Operacao.java
@@ -0,0 +1,7 @@
+public class Operacao {
+    public static int soma(int a, int b){
+        int s = a + b;
+        System.out.println(s);
+        return s;
+    }
+}
