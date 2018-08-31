41df84844f22f3d516637c46da66aad45dd58ab1
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-31 01:27:29 -0300

    chamada de classe exterior

---
+++ classeExterior/src/Home.java
@@ -0,0 +1,6 @@
+public class Home {
+    public static void main(String[] args) {
+        int r = Operacoes.soma(22, 13);
+        System.out.println(r);
+    }
+}
---
+++ classeExterior/src/Operacoes.java
@@ -0,0 +1,6 @@
+public class Operacoes {
+    public static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+}
