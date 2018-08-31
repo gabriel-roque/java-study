60c34f12626165e609362f29ced33ba323c0bec2
gabriel-roque <gabrielrms.dll@gmail.com>
2018-08-31 01:59:42 -0300

    remove

--- chamandoClasses/src/Home.java
+++
@@ -1,9 +0,0 @@
-public class Home {
-    public static void main(String[] args) {
-        /* chamando classe exterior
-            se faz necessario especificar o nome da classe
-            e o metodo que queremos solicitar
-         */
-        int r = Operacao.soma(22, 33);
-    }
-}
--- chamandoClasses/src/Operacao.java
+++
@@ -1,7 +0,0 @@
-public class Operacao {
-    public static int soma(int a, int b){
-        int s = a + b;
-        System.out.println(s);
-        return s;
-    }
-}
