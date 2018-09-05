4d9f3caa169b91283fa4642d3187d1b9e1574e7f
gabriel-roque <gabrielrms.dll@gmail.com>
2018-09-04 23:52:38 -0300

    JOptionPane Image

---
+++ swing/src/AddImage.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class AddImage {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "texto da caixa","titulo da caixa",JOptionPane.INFORMATION_MESSAGE,new ImageIcon("swing/src/dog.png"));
+    }
+}
