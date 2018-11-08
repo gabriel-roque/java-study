93213e866102d47eb796a5cb828d3db6a0d2acba
gabriel-roque <gabrielrms.dll@gmail.com>
2018-11-08 20:43:37 -0200

    Desafio 1 e 2 Swing finalizado

--- swingFaculdade/src/desafio01Swing/Desafio01.java
+++ swingFaculdade/src/desafio01Swing/Desafio01.java
@@ -4,16 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Desafio01 {
+
     public static void main(String[] args) {
+        startSystem();
+    }
 
-        /*todo refatorar codigo*/
-
-        // FRAME
+    private static JFrame montarFrame() {
 
         JFrame f = new JFrame("DESAFIO 01 SWING");
+        return f;
 
+    }
 
-        // MENU BAR
+    private static void menuBar(JFrame f) {
 
         JMenu menuArquivo = new JMenu("Arquivo");
         JMenu menuSobre = new JMenu("Sobre");
@@ -23,87 +26,96 @@ public class Desafio01 {
             JMenuItem s1 = new JMenuItem("Salvar");
             JMenuItem s2 = new JMenuItem("Salvar como");
 
-        subMenuSalvar.add(s1); subMenuSalvar.add(s2);
-        menuArquivo.add(a1); menuArquivo.add(subMenuSalvar);
-        menuArquivo.addSeparator(); menuArquivo.add(e1);
+            subMenuSalvar.add(s1);
+            subMenuSalvar.add(s2);
+            menuArquivo.add(a1);
+            menuArquivo.add(subMenuSalvar);
+            menuArquivo.addSeparator();
+            menuArquivo.add(e1);
+
 
         JMenuBar menuCompleto = new JMenuBar();
             menuCompleto.add(menuArquivo);
             menuCompleto.add(menuSobre);
             f.setJMenuBar(menuCompleto);
 
+    }
 
-        // TOP
+    private static void componentesTop(JFrame f) {
 
         JLabel lMat = new JLabel("Matricula Aluno:");
             lMat.setBounds(15, 10, 130, 15);
 
         JTextField txMat = new JTextField();
-            txMat.setBounds(120, 10, 60,17);
+            txMat.setBounds(120, 10, 60, 17);
 
 
         JLabel lTur = new JLabel("Turma do Aluno:");
             lTur.setBounds(200, 10, 130, 15);
 
         JTextField txTur = new JTextField();
-            txTur.setBounds(310, 10, 60,17);
+            txTur.setBounds(310, 10, 60, 17);
 
 
         f.add(lMat); f.add(txMat);
         f.add(lTur); f.add(txTur);
 
 
-        // SEPARATOR
+    }
+
+    private static void montarSeparadorTop(JFrame f) {
+
         JSeparator sep01 = new JSeparator();
             sep01.setBounds(15, 35, 355, 1);
 
         f.add(sep01);
 
+    }
 
-        // FORM MID
+    private static void componetesMid(JFrame f) {
 
-        int larguraD = 290;
+        int larguraD = 290; //DEFAULT COMP WIGHT
 
-            // Nome
+        // Nome
 
         JLabel lNome = new JLabel("Nome:");
             lNome.setBounds(15, 45, 130, 15);
 
         JTextField txNome = new JTextField();
-            txNome.setBounds(80, 45, larguraD,17);
+            txNome.setBounds(80, 45, larguraD, 17);
 
-            // Endereco
+        // Endereco
 
         JLabel lEnde = new JLabel("Endereco:");
             lEnde.setBounds(15, 65, 130, 15);
 
         JTextField txEnde = new JTextField();
-            txEnde.setBounds(80, 65, larguraD,17);
+            txEnde.setBounds(80, 65, larguraD, 17);
 
-            // Telefone
+        // Telefone
 
         JLabel lTel = new JLabel("Telefone:");
             lTel.setBounds(15, 85, 130, 15);
 
         JTextField txTel = new JTextField();
-            txTel.setBounds(80, 85, larguraD,17);
+            txTel.setBounds(80, 85, larguraD, 17);
 
-            // CPF
+        // CPF
 
         JLabel lCpf = new JLabel("CPF:");
             lCpf.setBounds(15, 105, 130, 15);
 
         JTextField lxCpf = new JTextField();
-            lxCpf.setBounds(80, 105, larguraD,17);
+            lxCpf.setBounds(80, 105, larguraD, 17);
 
 
         // RADIO BUTTON
 
-            JRadioButton rAluno = new JRadioButton("Aluno");
-                rAluno.setBounds(120,135, 60,15);
+        JRadioButton rAluno = new JRadioButton("Aluno");
+            rAluno.setBounds(120, 135, 60, 15);
 
-            JRadioButton rExAl = new JRadioButton("Ex Aluno");
-                rExAl.setBounds(190,135, 80,15);
+        JRadioButton rExAl = new JRadioButton("Ex Aluno");
+            rExAl.setBounds(190, 135, 80, 15);
 
 
         f.add(lNome); f.add(txNome);
@@ -113,14 +125,18 @@ public class Desafio01 {
         f.add(rAluno); f.add(rExAl);
 
 
-        // SEPARATOR
+    }
+
+    private static void montarSeparadorMid(JFrame f) {
+
         JSeparator sep02 = new JSeparator();
             sep02.setBounds(15, 165, 355, 1);
 
         f.add(sep02);
 
+    }
 
-        // BUTTON
+    private static void btnFinalForm(JFrame f) {
 
         JButton b1 = new JButton("OK!");
             b1.setBounds(50, 185, 60, 20);
@@ -133,14 +149,28 @@ public class Desafio01 {
 
         f.add(b1); f.add(b2); f.add(b3);
 
-        // CONFIG
+    }
 
-        f.setSize(385,280);
+    private static void configFrame(JFrame f) {
+
+        f.setSize(385, 280);
         f.setResizable(false);
         f.setLayout(null);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    }
+
+    private static void startSystem() {
+
+        JFrame f = montarFrame();
+        menuBar(f);
+        componentesTop(f);
+        montarSeparadorTop(f);
+        componetesMid(f);
+        montarSeparadorMid(f);
+        btnFinalForm(f);
+        configFrame(f);
 
     }
 }
